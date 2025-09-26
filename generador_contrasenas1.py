f380de1a1e6afe5a8e808210215ae8625c8bbff8
Luis Miguel Vasconez <lmvasconez@MacBook-Air-de-Luis-Miguel.local>
2025-09-26 12:40:07 -0500

    Borre duplicado

--- generador_contrasenas1.py
+++ generador_contrasenas1.py
@@ -8,6 +8,7 @@ def generar_contrasena(longitud=12):
     caracteres = string.ascii_letters + string.digits + string.punctuation
     return ''.join(secrets.choice(caracteres) for _ in range(longitud))
 
+## Deberia borrar esto me parece, pq al fin y al cabo, no quiero que se genere una contraseña con longitud fija, sino que quiero que el usuario determine ese parametro.
 if __name__ == "__main__":
     contraseña = generar_contrasena()
     print("Contraseña generada:", contraseña)
@@ -26,7 +27,10 @@ def pedir_longitud():
         else:
             print("Error: debe ingresar un número entero válido.")
 
+## Integrar parametros con funcion de generacion
+
 if __name__ == "__main__":
     longitud = pedir_longitud() #Con esto pido la longitud antes de generar la contraseña
     contraseña = generar_contrasena(longitud) #Con esto uso la longitud que el ususario indica
     print("Contraseña generada:", contraseña)
+
